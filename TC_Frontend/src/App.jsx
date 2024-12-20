c74187275f3d478da0fec964b8d57ac09b4c66eb
shikagin <125314835+shikagin@users.noreply.github.com>
2024-12-20 22:24:28 +0100

    Adding the frontend file

---
+++ TC_Frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+
+export default function App() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+
+      
+      {/* <Navbar />
+      <Routes>
+        <Route path ="/"  element={} />
+        <Route path="/" element= {}/>
+        <Route path="/" element={} />
+        <Route path="/" element={} />
+        <Route path="/" element={} />
+      </Routes> 
+      <Footer /> */}
+    </div>
+  )
+}
